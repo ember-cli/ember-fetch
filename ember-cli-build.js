e0b0c893c7b3750afbee7b04a00a8a23c82d477d
Aaron Quamme <aaron.quamme@gmail.com>
2016-01-31 16:34:11 -0600

    Upgrade dependencies
    Upgrade ember-cli to 1.13.15, update related dependencies
    and generated files based on the output of `ember init`.
    
    Upgrade fetch polyfill to 0.11.0 release.

---
+++ ember-cli-build.js
@@ -0,0 +1,8 @@
+/*jshint node:true*/
+/* global require, module */
+var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+
+module.exports = function(defaults) {
+  var app = new EmberAddon(defaults, {});
+  return app.toTree();
+};

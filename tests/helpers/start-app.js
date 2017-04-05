c6af27df2d36f3c90322687e4dabc64bcd19b747
Paul Chavard <git@paul.chavard.net>
2017-04-04 17:03:52 -0700

    update ember-cli

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -3,17 +3,13 @@ import Application from '../../app';
 import config from '../../config/environment';
 
 export default function startApp(attrs) {
-  let application;
-  let assign = Ember.assign || Ember.merge;
+  let attributes = Ember.merge({}, config.APP);
+  attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
 
-  // use defaults, but you can override
-  let attributes = assign({}, config.APP, attrs);
-
-  Ember.run(() => {
-    application = Application.create(attributes);
+  return Ember.run(() => {
+    let application = Application.create(attributes);
     application.setupForTesting();
     application.injectTestHelpers();
+    return application;
   });
-
-  return application;
 }

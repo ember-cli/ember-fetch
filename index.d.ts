c8aecab3d93bb41db294e1c3a34feb33fcefd46a
Chris Krycho <chris@chriskrycho.com>
2017-12-05 20:38:33 -0700

    Switch typings to ES-style module

--- index.d.ts
+++ index.d.ts
@@ -3,8 +3,6 @@
 // Definitions by: Toran Billups <https://github.com/toranb>
 // TypeScript Version: 2.3
 
-export = fetch;
-
-declare namespace fetch {
+declare module 'fetch' {
   export default function fetch(input: RequestInfo, init?: RequestInit): Promise<Response>;
 }

cbe37fa9a55da5910323e0876858b248df38ec5a
Thomas Wang <wangxgwxg@gmail.com>
2018-07-14 22:23:09 -0700

    unloop in dummy app
    For fixing "Error: Assertion Failed: You have turned on testing mode,
    which disabled the run-loop's autorun. You will need to wrap any code
    with asynchronous side-effects in a run" in CI.

--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -1,4 +1,5 @@
 import Controller from '@ember/controller';
+import { run } from '@ember/runloop';
 import fetch from 'fetch';
 
 export default Controller.extend({
@@ -6,17 +7,17 @@ export default Controller.extend({
     fetchSlowData() {
       fetch('/slow-data.json')
         .then((r) => r.json(), (e) => {
-          this.set('fetchedSlowDataFailed', true);
+          run(() => this.set('fetchedSlowDataFailed', true));
           throw e;
         })
-        .then((data) => this.setProperties({ fetchedSlowData: data, fetchedSlowDataFailed: false }));
+        .then((data) => {
+          run(() => this.setProperties({ fetchedSlowData: data, fetchedSlowDataFailed: false }));
+        });
     },
 
     badFetch() {
       fetch('http://localhost:9999') // probably there is nothing listening here :D
-        .then((r) => r.json(), () => {
-          this.set('fetchFailed', true);
-        });
+        .then((r) => r.json(), () => run(() => this.set('fetchFailed', true)));
     }
   }
 });

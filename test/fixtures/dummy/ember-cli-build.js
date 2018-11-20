9a1c3e394b1629d8733e280cca845e854e3e1c76
Thomas Wang <wangxgwxg@gmail.com>
2018-11-20 13:13:57 -0800

    Setup addon tests
    use npm run instead of yarn in travis to work around
    https://github.com/tomdale/ember-cli-addon-tests/issues/198

---
+++ test/fixtures/dummy/ember-cli-build.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function(defaults) {
+  let app = new EmberApp(defaults, {
+    // Add options here
+    'ember-fetch': {
+    }
+  });
+  return app.toTree();
+};

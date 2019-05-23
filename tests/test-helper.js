c52f2d87ccfceb71669676d4a5ef4a376bfb2e86
Thomas Wang <wangxgwxg@gmail.com>
2019-05-22 23:48:08 -0700

    Deprecate adapterFetch mixin

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,7 +2,14 @@ import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import { registerDeprecationHandler } from '@ember/debug';
 
 setApplication(Application.create(config.APP));
+
+registerDeprecationHandler((msg, options, next) => {
+  if (options.id !== 'deprecate-fetch-ember-data-support') {
+    next(msg, options);
+  }
+})
 
 start();

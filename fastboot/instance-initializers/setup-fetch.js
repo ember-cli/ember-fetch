8fdf660728054a5ad36e32444652ba0f3ac8bc25
Thomas Wang <wangxgwxg@gmail.com>
2019-10-25 18:05:36 -0700

    save request instead of protocol, host

--- fastboot/instance-initializers/setup-fetch.js
+++ fastboot/instance-initializers/setup-fetch.js
@@ -2,19 +2,12 @@ import { setupFastboot } from 'fetch';
 
 /**
  * To allow relative URLs for Fastboot mode, we need the per request information
- * from the fastboot service. Then we set the protocol and host to fetch module.
+ * from the fastboot service. Then we save the request from fastboot info.
+ * On each fetch with relative url we get host and protocol from it.
  */
 function patchFetchForRelativeURLs(instance) {
   const fastboot = instance.lookup('service:fastboot');
-  const request = fastboot.get('request');
-  // Prember is not sending protocol
-  const protocol = request.protocol === 'undefined:' ? 'http:' : request.protocol;
-  try {
-    // host is cp
-    setupFastboot(protocol, request.get('host'));
-  } catch (_e) {
-    // Do not throw error until request with a relative url.
-  }
+  setupFastboot(fastboot.get('request'));
 }
 
 export default {

de60890dbe7986ca7a21aaa3ca33baca1bd1edbf
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-11-06 10:54:33 -0500

    Set the ember-cli versions for older ember

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -59,6 +59,7 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~3.28.0',
+            'ember-cli': '~3.28.0',
           },
         },
       },
@@ -67,6 +68,7 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~4.4.0',
+            'ember-cli': '~4.4.0',
           },
         },
       },
@@ -75,6 +77,7 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~4.8.0',
+            'ember-cli': '~4.8.0',
           },
         },
       },
@@ -83,6 +86,7 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~4.12.0',
+            'ember-cli': '~4.12.0',
           },
         },
       },

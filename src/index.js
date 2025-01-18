7abae8fb0425b62318df82f7ebc337fc77308744
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-01-18 11:41:46 -0500

    ope, lints

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 import { waitForPromise } from '@ember/test-waiters';
 
 export async function wrappedFetch(...args) {
-    let responsePromise = fetch(...args);
+  let responsePromise = fetch(...args);
 
-    waitForPromise(responsePromise);
+  waitForPromise(responsePromise);
 
-    let response = await responsePromise;
+  let response = await responsePromise;
 
-    return new Proxy(response, {
-        get(target, prop, receiver) {
-            let original = Reflect.get(target, prop, receiver);
+  return new Proxy(response, {
+    get(target, prop, receiver) {
+      let original = Reflect.get(target, prop, receiver);
 
-            if (['json', 'text', 'arrayBuffer', 'blob', 'formData'].includes(prop)) {
-                return (...args) => {
-                    let parsePromise = original(...args);
+      if (['json', 'text', 'arrayBuffer', 'blob', 'formData'].includes(prop)) {
+        return (...args) => {
+          let parsePromise = original(...args);
 
-                    return waitForPromise(parsePromise);
-                }
-            }
+          return waitForPromise(parsePromise);
+        };
+      }
 
-            return original;
-        }
-    });
+      return original;
+    },
+  });
 }
 
 export default wrappedFetch;
